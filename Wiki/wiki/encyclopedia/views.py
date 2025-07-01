44d12da0ae278f55a4149e9aa55e39ce023940a8
Sofilove3 <saap1509@gmail.com>
2025-07-01 10:47:13 +0200

    working on Entry Page

--- Wiki/wiki/encyclopedia/views.py
+++ Wiki/wiki/encyclopedia/views.py
@@ -7,4 +7,24 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+'''
+def title(request, title):
+    if title.capitalize() in util.list_entries():
+        return render(request, "encyclopedia/title.html", {
+            "content": util.get_entry(title),
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+        })
+'''
+def title(request, title):
+    #logic is not working...
+    if title not in util.list_entries():
+        return render(request, "encyclopedia/error.html", {
+        })
+    else:
+        return render(request, "encyclopedia/title.html", {
+            "content": util.get_entry(title),
+            "title": title
+        })
