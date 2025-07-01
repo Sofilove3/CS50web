5179140e80fdaf240034e5de00f98168ec355d0b
Sofilove3 <saap1509@gmail.com>
2025-07-01 11:21:16 +0200

    Index Page done

--- Wiki/wiki/encyclopedia/views.py
+++ Wiki/wiki/encyclopedia/views.py
@@ -7,19 +7,8 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-'''
+
 def title(request, title):
-    if title.capitalize() in util.list_entries():
-        return render(request, "encyclopedia/title.html", {
-            "content": util.get_entry(title),
-            "title": title
-        })
-    else:
-        return render(request, "encyclopedia/error.html", {
-        })
-'''
-def title(request, title):
-    #logic is not working...
     if title.casefold() not in (x.casefold() for x in util.list_entries()):
         return render(request, "encyclopedia/error.html", {
         })
@@ -28,5 +17,3 @@ def title(request, title):
             "content": util.get_entry(title),
             "title": title
         })
-
-# if 'MICHAEL89'.casefold() in (name.casefold() for name in USERNAMES):
