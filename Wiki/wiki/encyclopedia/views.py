38bca2ed6dcc979ea6c1e90a8b31169a13b99385
Sofilove3 <saap1509@gmail.com>
2025-07-02 11:33:51 +0200

    New Page done

--- Wiki/wiki/encyclopedia/views.py
+++ Wiki/wiki/encyclopedia/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 from . import util
 
+from django import forms
+
+from django.urls import reverse
+
+from django.http import HttpResponseRedirect
+
+class NewPageForm(forms.Form):
+    page = forms.CharField(label="Name Page")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -38,4 +46,29 @@ def search(request):
         "search": query
     })
 
-        
+def new(request):
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewPageForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            page = form.cleaned_data["page"]
+            content = request.POST["body_text"]
+
+            if util.get_entry(page):
+                return render(request, "encyclopedia/page_already_exsist.html", {
+                    "page": page
+            })
+            else:
+                util.save_entry(page, content)
+                return render(request, "encyclopedia/index.html", {
+                    "entries": util.list_entries()
+                })
+
+    return render(request, "encyclopedia/new.html", {
+        "form": NewPageForm()
+    })
